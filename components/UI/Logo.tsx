bf319ca6a0502c45685aa57a426bca3f63c24151
elfajriharsuda <elfajri.harsuda@gmail.com>
2024-07-17 14:49:58 +0700

    add logo component

---
+++ components/UI/Logo.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Image from "next/image";
+import logo from "../../public/images/illustration/student/128/white.svg";
+import Link from "next/link";
+const Logo = () => {
+  return (
+    <div className="logo flex">
+      <Link href="/" className="flex items-center ">
+        <div className="avatar-title bg-[#5567ff] rounded	p-1">
+          <Image src={logo} alt="logo image" width={40} height={40}></Image>
+        </div>
+        <div className=" p-1 font-bold">Luma</div>
+      </Link>
+    </div>
+  );
+};
+
+export default Logo;
