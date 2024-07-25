2ae6a65def7a5718a11ed8f9be6de8f0b8ba1a5a
elfajriharsuda <elfajri.harsuda@gmail.com>
2024-07-25 11:53:37 +0700

    add Login Button

---
+++ components/UI/LoginButton.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function LoginButton() {
+  return (
+    <button className="h-fit rounded-2xl text-neutral-400 px-4 py-2	transition-all hover:bg-neutral-400 hover:text-white hover:border-2 hover:text-neutral-400/90">
+      Login
+    </button>
+  );
+}
