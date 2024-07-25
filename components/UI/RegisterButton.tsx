a8585f10dd73119ceb690391630319ed3088b2c3
elfajriharsuda <elfajri.harsuda@gmail.com>
2024-07-25 11:54:03 +0700

    add Register Button

---
+++ components/UI/RegisterButton.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function RegisterButton() {
+  return (
+    <button className="h-fit rounded-2xl border-2 border-neutral-400 text-neutral-400 px-4 py-2	transition-all hover:border-black hover:text-black/90">
+      Register
+    </button>
+  );
+}
