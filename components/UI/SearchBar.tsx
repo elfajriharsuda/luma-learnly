6d824454eab8fc6a8fc86bde01180a7795c0e4c0
elfajriharsuda <elfajri.harsuda@gmail.com>
2024-07-25 11:52:02 +0700

    modify searchBar Component

--- components/UI/SearchBar.tsx
+++ components/UI/SearchBar.tsx
@@ -3,11 +3,15 @@ import search from "../../public/images/icon/UI/search.svg";
 import Image from "next/image";
 const SearchBar = () => {
   return (
-    <form className="search-form bg-white rounded-2xl gap-5 p-2">
-      <button className="btn ">
+    <form className="search-form bg-neutral-100 rounded-2xl p-2 mx-5">
+      <button className="btn mx-2">
         <Image src={search} alt="search" width={15} height={15}></Image>
       </button>
-      <input type="text" className="p-1 border-none" placeholder="Search..." />
+      <input
+        type="text"
+        className="p-1 border-none bg-transparent outline-none	"
+        placeholder="Search..."
+      />
     </form>
   );
 };
